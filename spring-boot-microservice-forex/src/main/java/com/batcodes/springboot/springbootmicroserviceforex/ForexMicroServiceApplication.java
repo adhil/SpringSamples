9758d08fcc5c836f7ec9d42ad1e2ad866f0d3e78
adhil <adhilrahiman@gmail.com>
2018-06-11 07:15:49 +0530

    Spring boot samples

---
+++ spring-boot-microservice-forex/src/main/java/com/batcodes/springboot/springbootmicroserviceforex/ForexMicroServiceApplication.java
@@ -0,0 +1,12 @@
+package com.batcodes.springboot.springbootmicroserviceforex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForexMicroServiceApplication.class, args);
+	}
+}
